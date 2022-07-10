fce0976807b6c63ac53250d74107923c6c846fb4
Luiss Montoya <luisgoemon@gmail.com>
2022-07-09 22:37:11 -0500

    create_icons_description

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -10,8 +10,18 @@ class BasicDesignScreen extends StatelessWidget {
             image: AssetImage('assets/landscape.jpg'),
           ),
 
-          //Titulo
-          Title()
+          //Title
+          Title(),
+
+          //button section
+          ButtonSection(),
+
+          //Description
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+                'Excepteur quis aute in mollit velit sunt dolor dolor cupidatat amet cupidatat excepteur aliqua.Aliquip aliqua occaecat officia esse nostrud cupidatat consectetur exercitation excepteur in consectetur nostrud ipsum.'),
+          )
         ],
       ),
     );
@@ -48,6 +58,51 @@ class Title extends StatelessWidget {
           Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(icon: Icons.call, texto: 'Call'),
+          CustomButton(icon: Icons.map_sharp, texto: 'Route'),
+          CustomButton(icon: Icons.share, texto: 'Shared')
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String texto;
+
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.texto,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(this.icon, color: Colors.blue),
+        Text(
+          this.texto,
+          style: TextStyle(color: Colors.blue),
+        )
+      ],
     );
   }
 }
