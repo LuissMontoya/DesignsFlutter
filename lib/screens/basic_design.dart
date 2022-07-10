54df3ff423bdbb2cffc8840db3b04349fef824b2
Luiss Montoya <luisgoemon@gmail.com>
2022-07-09 22:09:36 -0500

    create_Title

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -4,8 +4,49 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg')),
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage('assets/landscape.jpg'),
+          ),
+
+          //Titulo
+          Title()
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Paisajes de Color',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Mensaje de bienvenida en pantalla',
+                style: TextStyle(color: Colors.black45),
+              ),
+            ],
+          ),
+          Expanded(child: Container()),
+          Icon(Icons.star, color: Colors.red),
+          Text('41')
+        ],
       ),
     );
   }
